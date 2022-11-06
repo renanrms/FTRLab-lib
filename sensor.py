99f734be2e083528f79adddb72209457f272ca1f
Renan Passos <renanpassos@poli.ufrj.br>
2022-11-06 00:02:04 -0300

    Versão para testar a comunicação.

---
+++ sensor.py
@@ -0,0 +1,15 @@
+class Sensor:
+  def __init__(self, module, ports):
+    # TODO: adicionar kwargs para opções extrar a serem passadas ao módulo
+    self.module = module.Module(ports)
+    self.isActive = True # False
+    self.measurements = []
+
+  def doMeasurement(self):
+    # TODO: possivelmente adicionar mutex para evitar problema de concorrência se isso virar algo assíncrono
+    self.measurements.append(self.module.measurement())
+  
+  def get(self):
+    tmp = [*self.measurements]
+    self.measurements = []
+    return tmp
