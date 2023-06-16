916ecb62c45e0c5a1081b5bc15eadb99278c489f
Renan Passos <renanpassos@poli.ufrj.br>
2023-06-16 12:15:55 -0300

    Adiciona capacidade de lidar com falha em medição de um sensor.

--- lib/FTRLab/src/Board/takeMeasurement.cpp
+++ lib/FTRLab/src/Board/takeMeasurement.cpp
@@ -5,11 +5,18 @@ void Board::takeMeasurement(Sensor *sensor, unsigned index)
   int64_t t1, t2;
   double timestamp;
 
-  t1 = NTP.micros();
-  String measure = sensor->takeMeasure();
-  t2 = NTP.micros();
+  try
+  {
+    t1 = NTP.micros();
+    String measure = sensor->takeMeasure();
+    t2 = NTP.micros();
 
-  timestamp = ((double)(t1 + t2) / 2) / 1000000.0;
+    timestamp = ((double)(t1 + t2) / 2) / 1000000.0;
 
-  this->measurements.push({index, timestamp, measure});
+    this->measurements.push({index, timestamp, measure});
+  }
+  catch (std::exception error)
+  {
+    Serial.println("Measurement error from sensor " + String(index) + ".");
+  }
 }
