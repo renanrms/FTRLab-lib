937f432446c2b55ec842a99cc6ce6c680a5c1fe7
Renan Passos <renanpassos@poli.ufrj.br>
2023-07-22 18:21:47 -0300

    Cria um limite de tempo oara a sincronização de relógio.
    Também cria um parâmetro no dispositivo para indicar se seu relógio está sincronizado.

--- lib/FTRLab/src/Board/setupNTP.cpp
+++ lib/FTRLab/src/Board/setupNTP.cpp
@@ -4,22 +4,33 @@ void Board::setupNTP()
 {
   NTP.begin("pool.ntp.org", false);
 
-  Serial.print("\nSynchronizing device clock with NTP ");
+  Serial.println("\nSynchronizing device clock with NTP. Timeout is " + String(NTP_SYNC_WAIT_TIME) + "s");
 
   // NTPStatus_t possible values:
   // syncd = 0       -> Time synchronized correctly
   // unsyncd = -1    -> Time may not be valid
   // partialSync = 1 -> NPT is synchronised but precission is below threshold
-  while (NTP.syncStatus() == -1)
+
+  time_t NtpSyncStart = NTP.getUptime();
+  while (NTP.syncStatus() == -1 && NTP.getUptime() - NtpSyncStart < NTP_SYNC_WAIT_TIME)
   {
     digitalWrite(this->pins.networkLed, HIGH);
-    delay(950);
+    delay(1950);
     digitalWrite(this->pins.networkLed, LOW);
     delay(50);
-    Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("First sync done.");
+  if (NTP.syncStatus() != -1)
+  {
+    this->timeSynced = true;
+    Serial.println("First sync done.");
+  }
+  else
+  {
+    // Para as tentativas de sincronização para não gerar descontinuidade nas medidas.
+    NTP.stop();
+    Serial.println("NTP response not received. It's possible the network don't have acess to internet.");
+    Serial.println("Will continue without global time configured.");
+  }
   Serial.println(String("Time: ") + NTP.getTimeDateStringUs());
 }
