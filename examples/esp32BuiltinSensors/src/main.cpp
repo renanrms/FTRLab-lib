11162579332fe0901df3c218a415a8209195c03b
Renan Passos <renanpassos@poli.ufrj.br>
2023-08-01 02:59:31 -0300

    Tranforma repositório para forma de biblioteca.

--- examples/esp32BuiltinSensors/src/main.cpp
+++ examples/esp32BuiltinSensors/src/main.cpp
@@ -1,7 +1,6 @@
 #include "FTRLab.hpp"
 
 #include "sensors/HallEffectSensor.cpp"
-#include "sensors/UltrasonicSensor.cpp"
 #include "sensors/TemperatureBuiltInSensor.cpp"
 
 Preferences ftrlabPreferences;
@@ -10,25 +9,21 @@ Preferences ftrlabPreferences;
  * Pinos GPIO utilizados no dispositivo.
  * Configure-os de acordo com a necessidade, observando pinos obrigatórios e restrições de hardware para os pinos escolhidos em cada função.
  */
-enum DEVICE_PINS
+enum PINS
 {
   NETWORK_RECONFIGURATION_BUTTON = 5,
   NETWORK_STATUS_LED = 18,
-  // Adicione aqui outros pinos utilizados pela placa
-  ULTRASONIC_SENSOR_TRIGGER = 12,
-  ULTRASONIC_SENSOR_ECHO = 13,
 };
 
 void setup()
 {
   // Configuração de sensores, informações e pinos escolhidos para o dispositivo
-  board.setName("Física Básica");
-  // board.addSensor(new HallEffectSensor());
-  // board.addSensor(new TemperatureBuiltInSensor());
-  board.addSensor(new UltrasonicSensor(DEVICE_PINS::ULTRASONIC_SENSOR_TRIGGER, DEVICE_PINS::ULTRASONIC_SENSOR_ECHO));
+  board.setName("ESP32 Builtin");
+  board.addSensor(new HallEffectSensor());
+  board.addSensor(new TemperatureBuiltInSensor());
   board.setDevicePins(
-      DEVICE_PINS::NETWORK_RECONFIGURATION_BUTTON,
-      DEVICE_PINS::NETWORK_STATUS_LED);
+      PINS::NETWORK_RECONFIGURATION_BUTTON,
+      PINS::NETWORK_STATUS_LED);
 
   // Se necessário descomente as linhas abaixo e altere os valores para ajustar períodos de medição e envio
   // board.setMinimumMeasurementPeriod(10000);
