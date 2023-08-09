c45b3097d79b3530cca987dac564330497979037
Renan Passos <renanpassos@poli.ufrj.br>
2023-08-09 00:38:35 -0300

    Corrige valor tamanho da pilha.

--- src/Board/setup.cpp
+++ src/Board/setup.cpp
@@ -23,7 +23,7 @@ void Board::setup()
   xTaskCreatePinnedToCore(
       communicationHandlerWrapper,
       "COMM",
-      1000,
+      10000,
       NULL,
       tskIDLE_PRIORITY,
       &communicationTask,
@@ -32,7 +32,7 @@ void Board::setup()
   xTaskCreatePinnedToCore(
       measurementHandlerWrapper,
       "MEAS",
-      1000,
+      10000,
       NULL,
       configMAX_PRIORITIES,
       &measurementTask,
