dc7a8505c9a3f4c153ff8bb65e16238be886af7d
Renan Passos <renanpassos@poli.ufrj.br>
2023-08-09 04:27:46 -0300

    Aumenta o Payload maximo enviado.

--- include/FTRLab/constants.hpp
+++ include/FTRLab/constants.hpp
@@ -8,7 +8,7 @@
  * Constantes terminadas em "SIZE", são tamanhos totais (consideram o caractere de terminação).
  */
 
-const unsigned PAYLOAD_MAX_LENGTH = 1024;
+const unsigned PAYLOAD_MAX_LENGTH = 1400;
 // const unsigned MEASUREMENT_OVERHEAD_LENGTH = 21;
 const unsigned MEASUREMENT_MAX_LENGTH = 100; // Valor majorado
 const unsigned MEASUREMENT_MAX_SIZE = MEASUREMENT_MAX_LENGTH + 1;
@@ -17,8 +17,8 @@ const unsigned MEASUREMENT_MAX_SIZE = MEASUREMENT_MAX_LENGTH + 1;
 const unsigned BATTERY_INFO_MAX_LENGTH = 38;
 const unsigned BATTERY_INFO_MAX_SIZE = BATTERY_INFO_MAX_LENGTH + 1;
 const time_t NTP_SYNC_WAIT_TIME = 90;
-const int64_t DEFAULT_TAKEING_PERIOD = 20;  // Unit: ms
-const int64_t DEFAULT_SENDING_PERIOD = 200; // Unit: ms
+const int64_t DEFAULT_TAKEING_PERIOD = 12;   // Unit: ms
+const int64_t DEFAULT_SENDING_PERIOD = 1500; // Unit: ms
 
 const uint16_t PORT = 3333;
 const String PROTOCOL = "_tcp";
